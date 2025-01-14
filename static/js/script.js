db046be46151d961e6902e43e4979999c4d33c8b
Rosa Isela Miranda Perez <rm007522@gmail.com>
2025-01-14 15:08:51 -0600

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Función para obtener la lista de notebooks desde la API
 function fetchNotebooksList() {
-    fetch('https://apoper.onrender.com//documentos')
+    fetch('https://apoper.onrender.com/documentos')
         .then(response => response.json())
         .then(data => {
             const notebooksList = document.getElementById('notebooks-list');
@@ -31,49 +31,40 @@ function fetchNotebooksList() {
 
 // Función para obtener el contenido de un notebook
 function fetchNotebookContent(notebookName) {
-    fetch(`https://apoper.onrender.com//documentos/contenido/${notebookName}`)
+    fetch(`https://apoper.onrender.com/documentos/contenido/${notebookName}`)
         .then(response => response.json())
         .then(data => {
             const contentDiv = document.getElementById('content');
             contentDiv.innerHTML = ''; // Limpiar contenido previo
 
-            // Mostrar el contenido de las celdas
-            data.forEach(cell => {
-                const cellDiv = document.createElement('div');
-                if (cell.tipo === 'código') {
-                    cellDiv.innerHTML = `
-                        <strong>Celda de Código:</strong>
-                        <pre>${cell.contenido}</pre>
-                    `;
+            // Si la respuesta es un mensaje de error
+            if (data.mensaje) {
+                contentDiv.innerHTML = `<p>${data.mensaje}</p>`;
+                return;
+            }
 
-                    // Mostrar las salidas
-                    cell.salidas.forEach(salida => {
-                        if (salida.tipo === 'texto') {
-                            cellDiv.innerHTML += `
-                                <strong>Salida (Texto):</strong>
-                                <pre>${salida.contenido}</pre>
-                            `;
-                        } else if (salida.tipo === 'imagen') {
-                            cellDiv.innerHTML += `
-                                <strong>Salida (Imagen):</strong>
-                                <img src="data:image/png;base64,${salida.contenido}" alt="Imagen de salida"/>
-                            `;
-                        } else if (salida.tipo === 'json') {
-                            cellDiv.innerHTML += `
-                                <strong>Salida (JSON):</strong>
-                                <pre>${JSON.stringify(salida.contenido, null, 2)}</pre>
-                            `;
-                        } else if (salida.tipo === 'html') {
-                            cellDiv.innerHTML += `
-                                <strong>Salida (HTML):</strong>
-                                <div>${salida.contenido}</div>
-                            `;
-                        }
-                    });
-                } else if (cell.tipo === 'texto') {
+            // Mostrar el contenido de las celdas
+            data.forEach(salida => {
+                const cellDiv = document.createElement('div');
+                if (salida.tipo === 'texto') {
                     cellDiv.innerHTML = `
-                        <strong>Celda de Markdown:</strong>
-                        <pre>${cell.contenido}</pre>
+                        <strong>Salida (Texto):</strong>
+                        <pre>${salida.contenido}</pre>
+                    `;
+                } else if (salida.tipo === 'imagen') {
+                    cellDiv.innerHTML = `
+                        <strong>Salida (Imagen):</strong>
+                        <img src="data:image/png;base64,${salida.contenido}" alt="Imagen de salida"/>
+                    `;
+                } else if (salida.tipo === 'json') {
+                    cellDiv.innerHTML = `
+                        <strong>Salida (JSON):</strong>
+                        <pre>${JSON.stringify(salida.contenido, null, 2)}</pre>
+                    `;
+                } else if (salida.tipo === 'html') {
+                    cellDiv.innerHTML = `
+                        <strong>Salida (HTML):</strong>
+                        <div>${salida.contenido}</div>
                     `;
                 }
                 contentDiv.appendChild(cellDiv);

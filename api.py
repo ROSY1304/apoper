48cbd451c7637b933631733a4772d5cb36098386
Rosa Isela Miranda Perez <rm007522@gmail.com>
2025-01-14 14:06:23 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify, request, send_from_directory
 import os
 import nbformat
@@ -31,26 +32,36 @@ def ver_contenido_documento(nombre):
     try:
         notebook_path = os.path.join(DOCUMENTS_FOLDER, nombre)
         
-        if os.path.exists(notebook_path) and nombre.endswith('.ipynb'):
+        if os.path.exists(notebook_path):
             with open(notebook_path, 'r', encoding='utf-8') as f:
                 notebook_content = nbformat.read(f, as_version=4)
 
-            # Filtrar celdas específicas
-            celdas_interes = [113, 126]
-            resultados_accuracy = []
+            resultados = []
 
-            for idx, cell in enumerate(notebook_content.cells, start=1):
-                if idx in celdas_interes and cell.cell_type == 'code':
-                    for output in cell.outputs:
-                        if 'text' in output and 'Accuracy' in output['text']:
-                            resultados_accuracy.append(output['text'].strip())
+            if nombre == 'regresion.ipynb':
+                # Procesar celdas específicas para regresión
+                celdas_interes = [113, 126]
+                for idx, cell in enumerate(notebook_content.cells, start=1):
+                    if idx in celdas_interes and cell.cell_type == 'code':
+                        for output in cell.outputs:
+                            if 'text' in output and 'Accuracy' in output['text']:
+                                resultados.append(output['text'].strip())
+            
+            elif nombre == 'arboles.ipynb':
+                # Procesar la celda 38 para árboles
+                celda_interes = 38
+                for idx, cell in enumerate(notebook_content.cells, start=1):
+                    if idx == celda_interes and cell.cell_type == 'code':
+                        for output in cell.outputs:
+                            if 'text' in output:
+                                resultados.append(output['text'].strip())
 
-            if not resultados_accuracy:
-                return jsonify({'mensaje': 'No se encontraron valores de Accuracy en las celdas seleccionadas.'}), 404
+            if not resultados:
+                return jsonify({'mensaje': 'No se encontraron resultados en las celdas seleccionadas.'}), 404
 
-            return jsonify(resultados_accuracy), 200
+            return jsonify(resultados), 200
         else:
-            return jsonify({'mensaje': 'Archivo no encontrado o formato incorrecto'}), 404
+            return jsonify({'mensaje': 'Archivo no encontrado o formato incorrecto.'}), 404
     except Exception as e:
         return jsonify({'mensaje': str(e)}), 500
 

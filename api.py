600d26bc9d879c2176ac6d6652abea54fc3ee791
Rosa Isela Miranda Perez <rm007522@gmail.com>
2025-01-14 14:49:34 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory, render_template
 import os
 import nbformat
 from flask_cors import CORS
@@ -30,9 +30,10 @@ def ver_contenido_documento(nombre):
         if os.path.exists(notebook_path) and nombre.endswith('.ipynb'):
             with open(notebook_path, 'r', encoding='utf-8') as f:
                 notebook_content = nbformat.read(f, as_version=4)
-            
+
             accuracy_results = []
-            for cell in notebook_content.cells:
+            decision_tree_result = None
+            for index, cell in enumerate(notebook_content.cells):
                 if cell.cell_type == 'code':
                     for output in cell.outputs:
                         if 'text' in output:
@@ -40,11 +41,16 @@ def ver_contenido_documento(nombre):
                             for line in lines:
                                 if 'accuracy' in line.lower():
                                     accuracy_results.append(line.strip())
+                                if nombre == 'decision_tree.ipynb' and index == 38:
+                                    if 'image/png' in output['data']:
+                                        decision_tree_result = output['data']['image/png']
             
-            if not accuracy_results:
+            if nombre == 'decision_tree.ipynb' and decision_tree_result:
+                return jsonify({'resultado': decision_tree_result}), 200
+            elif not accuracy_results:
                 return jsonify({'mensaje': 'No se encontraron resultados de accuracy en el notebook'}), 404
-            
-            return jsonify(accuracy_results), 200
+            else:
+                return jsonify(accuracy_results), 200
         else:
             return jsonify({'mensaje': 'Archivo no encontrado o formato incorrecto'}), 404
     except Exception as e:
